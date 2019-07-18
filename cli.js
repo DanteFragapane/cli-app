9dd20d83fa6ed9c6dd4cae4148090745a2cdf51d
Dante Fragapane <dante.fragapane@gmail.com>
2019-07-18 18:51:39 -0400

    Parsed user inputs, started object of actions

--- cli.js
+++ cli.js
@@ -1,1 +1,11 @@
 const tvMazeUri = 'http://api.tvmaze.com'
+
+const action = process.argv.slice(2, 3)
+const query = process.argv.slice(3).join(' ')
+
+const actions = {
+  show: function (query) {},
+  actor: function (query) {}
+}
+
+actions[action](query)
