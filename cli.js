9f3b2406f5001e73cc3c46f849fd3ee7b263cc28
Dante Fragapane <dante.fragapane@gmail.com>
2019-07-18 19:21:38 -0400

    Working more on axios calls and proceeding logic

--- cli.js
+++ cli.js
@@ -1,11 +1,15 @@
+// Ipmorts and variables
 const tvMazeUri = 'http://api.tvmaze.com'
 const axios = require('axios')
 const fs = require('fs')
 const logFile = './log.txt'
 
-const action = process.argv.slice(2, 3)
-const query = process.argv.slice(3).join(' ')
+// Parsing arguments from CLI
+const arguments = process.argv.slice(2)
+const action = arguments.slice(0, 1)
+const query = arguments.slice(1).join(' ')
 
+// Logging function
 const log = function (message) {
   if (fs.existsSync(logFile)) {
     fs.appendFile(logFile, `\n${message}`, (err) => {
@@ -18,17 +22,30 @@ const log = function (message) {
   }
 }
 
+// All the actions the user can make and associated logic
 const actions = {
-  show: function (query) {},
+  // Show action
+  show: function (query) {
+    axios({
+      method: 'GET',
+      url: `${tvMazeUri}/singlesearch/shows`,
+      params: { q: query }
+    }).then((response) => {
+      console.log(response.data)
+    })
+  },
+
+  // Actor action
   actor: function (query) {
     axios({
       method: 'GET',
-      url: tvMazeUri + '/search/people',
+      url: `${tvMazeUri}/search/shows`,
       params: { q: query }
     }).then((response) => {
-      console.log(response.data[0])
+      console.log(response.data)
     })
   }
 }
 
+log(arguments)
 actions[action](query)
